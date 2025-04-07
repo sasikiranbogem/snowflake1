8195ea4cca98d342c42316ae724ba2b386e40fa1
SASIKIRAN <161295445+sasikiranbogem@users.noreply.github.com>
2025-04-07 09:57:07 +0530

    Add files via upload

---
+++ TT.sql
@@ -0,0 +1,3 @@
+I have involved in complex query implementations and performance tuning activities.
+Complex implementations like UDF creation for one of the function POS_EXPLODE() which exists in Hadoop 
+which generates the calender days between two business dates.Equallent function not there in snowflake .For this i have created UDF using Lateral join and generator function
